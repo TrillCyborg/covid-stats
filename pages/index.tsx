63052729281c111da2ba1789644a220f223b8624
Jason Werner <jason@trxrg.com>
2020-03-23 17:51:23 -0500

    add splash and new icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home = (props: { data: Data }) => {
   return (
     <div className="container">
       <Head>
-        <title>COVID-19 Stats</title>
+        <title>COVID-19</title>
         <link rel="icon" href="/favicon.ico" />
         {/* <link href="https://fonts.googleapis.com/css2?family=Quantico:wght@400;700&display=swap" rel="stylesheet"/> */}
         {/* <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet"/> */}
@@ -55,23 +55,135 @@ const Home = (props: { data: Data }) => {
           rel="stylesheet"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <meta name="apple-mobile-web-app-title" content="Banned.Video" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#1e2022" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/images/favicon/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/images/favicon/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/images/favicon/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/images/favicon/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/images/favicon/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/images/favicon/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/images/favicon/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-icon-180x180.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/images/favicon/android-icon-192x192.png"
+        />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+        <meta name="msapplication-TileImage" content="/images/favicon/ms-icon-144x144.png" />
+
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2048-2732.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2732-2048.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1668-2388.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2388-1668.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1668-2224.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2224-1668.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1536-2048.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2048-1536.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1242-2688.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2688-1242.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1125-2436.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2436-1125.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-828-1792.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1792-828.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1242-2208.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-2208-1242.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-750-1334.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1334-750.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-640-1136.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreen/apple-splash-1136-640.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+
         <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#060606" />
-        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#af0404" />
       </Head>
       <div id="page-wrapper">
         <Header {...props.data.usa} />
@@ -169,22 +281,22 @@ Home.getInitialProps = async () => {
   const usaData = await fetch('https://corona.lmao.ninja/countries/USA').then(res => res.json())
   const statesData = await fetch('https://corona.lmao.ninja/states').then(res => res.json())
   const allHistorical = await fetch('https://corona.lmao.ninja/historical').then(res => res.json())
-  const usaHistorical = await fetch('https://corona.lmao.ninja/historical/usa').then(res =>res.json())
-  const statesHistorical = allHistorical.filter(
-    ({ country, province }) =>
-      country === 'usa' && STATES.indexOf(nomalizeState(province)) !== -1
+  const usaHistorical = await fetch('https://corona.lmao.ninja/historical/usa').then(res =>
+    res.json()
   )
-  const allDates = Object.keys(usaHistorical.timeline.cases).map(date => moment(date, 'M/D/YY').valueOf())
+  const statesHistorical = allHistorical.filter(
+    ({ country, province }) => country === 'usa' && STATES.indexOf(nomalizeState(province)) !== -1
+  )
+  const allDates = Object.keys(usaHistorical.timeline.cases).map(date =>
+    moment(date, 'M/D/YY').valueOf()
+  )
   const filteredDates = allDates.filter(d => d > moment(START_DATE, 'M/D/YY').valueOf())
   statesData
     .filter(({ state }) => STATES.indexOf(nomalizeState(state)) !== -1)
     .forEach(state => {
       const id = nomalizeState(state.state)
-      const history = find(
-        statesHistorical,
-        ({ province }) => nomalizeState(province) === id
-      )
-      
+      const history = find(statesHistorical, ({ province }) => nomalizeState(province) === id)
+
       if (state.cases > mostInfected) {
         mostInfected = state.cases
       }
@@ -198,7 +310,7 @@ Home.getInitialProps = async () => {
             date: d,
             confirmed: parseInt(history.timeline.cases[date], 10),
             deaths: parseInt(history.timeline.deaths[date], 10),
-            recoveries: parseInt(history.timeline.recovered[date], 10)
+            recoveries: parseInt(history.timeline.recovered[date], 10),
           }
         }),
       } as DataItem
@@ -215,7 +327,7 @@ Home.getInitialProps = async () => {
             date: d,
             confirmed: parseInt(usaHistorical.timeline.cases[date], 10),
             deaths: parseInt(usaHistorical.timeline.deaths[date], 10),
-            recoveries: parseInt(usaHistorical.timeline.recovered[date], 10)
+            recoveries: parseInt(usaHistorical.timeline.recovered[date], 10),
           }
         }),
       } as DataItem,
