05922e95e8fcfe1c22aa060ffba80ac726d2a068
Jason Werner <jason@trxrg.com>
2020-03-24 23:34:33 -0500

    client side fetching

--- lib/api.ts
+++ lib/api.ts
@@ -5,30 +5,61 @@ import { START_DATE, STATES, STATE_MAP } from '../consts'
 
 // const serverEndpoint = 'https://api.covidstats.app'
 const serverEndpoint = 'https://corona.lmao.ninja'
-const covidTrackingEndpoint = 'https://covidtracking.com/api/'
 
-const getDataFromServer: () => Promise<Data> = async () => {
+const filterRelevantDates = (dates: string[]) =>
+  dates
+    .map(date =>
+      moment(date, 'M/D/YY')
+        .tz('Etc/GMT')
+        .valueOf()
+    )
+    .filter(
+      d =>
+        d >
+        moment(START_DATE, 'M/D/YY')
+          .tz('Etc/GMT')
+          .valueOf()
+    )
+
+const getUSData = async () => {
+  const usaData = await fetch(`${serverEndpoint}/countries/USA`).then(res => res.json())
+  return {
+    usa: {
+      ...usaData,
+      name: 'United States',
+    } as Partial<DataItem>,
+  } as Partial<Data>
+}
+
+const getUSChartData = async () => {
+  const usaHistorical = await fetch(`${serverEndpoint}/v2/historical/usa`).then(res => res.json())
+  const filteredDates = filterRelevantDates(Object.keys(usaHistorical.timeline.cases))
+  return {
+    usa: {
+      timeline: filteredDates.map(d => {
+        const date = moment(d)
+          .tz('Etc/GMT')
+          .format('M/D/YY')
+        return {
+          date: d,
+          confirmed: parseInt(usaHistorical.timeline.cases[date], 10),
+          deaths: parseInt(usaHistorical.timeline.deaths[date], 10),
+          // recoveries: parseInt(usaHistorical.timeline.recovered[date], 10),
+        }
+      }),
+    } as Partial<DataItem>,
+  } as Partial<Data>
+}
+
+const getStatesData = async () => {
   let mostInfected = 0
   const states = {} as { [key: string]: DataItem }
-  const usaData = await fetch(`${serverEndpoint}/countries/USA`).then(res => res.json())
   const statesData = await fetch(`${serverEndpoint}/states`).then(res => res.json())
   const allHistorical = await fetch(`${serverEndpoint}/historical`).then(res => res.json())
-  const usaHistorical = await fetch(`${serverEndpoint}/v2/historical/usa`).then(res => res.json())
   const statesHistorical = allHistorical.filter(
     ({ country, province }) => country === 'usa' && STATES.indexOf(nomalizeState(province)) !== -1
   )
-  const allDates = Object.keys(usaHistorical.timeline.cases).map(date =>
-    moment(date, 'M/D/YY')
-      .tz('Etc/GMT')
-      .valueOf()
-  )
-  const filteredDates = allDates.filter(
-    d =>
-      d >
-      moment(START_DATE, 'M/D/YY')
-        .tz('Etc/GMT')
-        .valueOf()
-  )
+  const filteredDates = filterRelevantDates(Object.keys(statesHistorical[0].timeline.cases))
   statesData
     .filter(({ state }) => STATES.indexOf(nomalizeState(state)) !== -1)
     .forEach(state => {
@@ -55,125 +86,25 @@ const getDataFromServer: () => Promise<Data> = async () => {
         }),
       } as DataItem
     })
+
   return {
     mostInfected,
-    usa: {
-      ...usaData,
-      name: 'United States',
-      timeline: filteredDates.map(d => {
-        const date = moment(d)
-          .tz('Etc/GMT')
-          .format('M/D/YY')
-        return {
-          date: d,
-          confirmed: parseInt(usaHistorical.timeline.cases[date], 10),
-          deaths: parseInt(usaHistorical.timeline.deaths[date], 10),
-          // recoveries: parseInt(usaHistorical.timeline.recovered[date], 10),
-        }
-      }),
-    } as DataItem,
     states,
-  } as Data
+  } as Partial<Data>
 }
 
-// const getDataFromCovidTracking: () => Promise<Data> = async () => {
-//   let mostInfected = 0
-//   const states = {} as { [key: string]: DataItem }
-//   const usaData = await fetch(`${covidTrackingEndpoint}/us`).then(res => res.json())
-//   const usaHistorical = await fetch(`${covidTrackingEndpoint}/us/daily`).then(res => res.json())
-//   const statesData = await fetch(`${covidTrackingEndpoint}/states`).then(res => res.json())
-//   const statesHistorical = await fetch(`${covidTrackingEndpoint}/states/daily`).then(res =>
-//     res.json()
-//   )
-//   const historical = statesHistorical.reduce((obj, item) => {
-//     if (!obj[item.state]) {
-//       obj[item.state] = []
-//     }
-//     obj[item.state].push(item)
-//     return obj
-//   }, {})
-//   const allDates = usaHistorical.map(({ date }) =>
-//     moment(date, 'YYYYMMDD')
-//       .tz('Etc/GMT')
-//       .valueOf()
-//   )
-//   const filteredDates = allDates.filter(
-//     d =>
-//       d >
-//       moment(START_DATE, 'M/D/YY')
-//         .tz('Etc/GMT')
-//         .valueOf()
-//   )
-//   statesData.forEach(state => {
-//     const id = STATE_MAP[state.state]
-//     const history = historical[state.state]
-
-//     if (id) {
-//       if (state.positive > mostInfected) {
-//         mostInfected = state.positive
-//       }
-
-//       states[id] = {
-//         ...state,
-//         name: denormalizeState(id),
-//         cases: usaData.positive,
-//         // todayCases?: number
-//         deaths: usaData.death,
-//         // todayDeaths?: number
-//         // recovered: number
-//         // todayRecovered?: number
-//         // active: number
-//         // critical?: number
-//         // casesPerOneMillion?: number
-//         timeline: filteredDates
-//           .reverse()
-//           .map((d, i) => {
-//             const date = history[i]
-//             if (date) {
-//               return {
-//                 date: d,
-//                 confirmed: parseInt(date.positive, 10),
-//                 deaths: parseInt(date.death, 10),
-//                 // recoveries: parseInt(date.recovered, 10),
-//               }
-//             }
-//             return null
-//           })
-//           .filter(d => !!d),
-//       } as DataItem
-//     }
-//   })
-//   return {
-//     mostInfected,
-//     usa: {
-//       ...usaData[0],
-//       name: 'United States',
-//       cases: usaData[0].positive,
-//       // todayCases?: number
-//       deaths: usaData[0].death,
-//       // todayDeaths?: number
-//       // recovered: number
-//       // todayRecovered?: number
-//       // active: number
-//       // critical?: number
-//       // casesPerOneMillion?: number
-//       timeline: filteredDates.reverse().map((d, i) => ({
-//         date: d,
-//         confirmed: parseInt(usaHistorical[i].positive, 10),
-//         deaths: parseInt(usaHistorical[i].death, 10),
-//         // recoveries: parseInt(usaHistorical[i].recovered, 10),
-//       })),
-//     } as DataItem,
-//     states,
-//   } as Data
-// }
-
-export const fetcher = async (url: string) => {
-  if (url === '/data') {
-    return getData()
-  } else {
-    return fetch(`${serverEndpoint}${url}`).then(res => res.json())
+export const fetcher = async (
+  url: string,
+  opts?: { body: string; method: string; noAuth?: boolean }
+) => {
+  switch (url) {
+    case '/us-data':
+      return getUSData()
+    case '/us-chart-data':
+      return getUSChartData()
+    case '/state-data':
+      return getStatesData()
+    default:
+      return {}
   }
 }
-
-export const getData = getDataFromServer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useWindowSize } from 'react-use'
+import useSWR from 'swr'
 import styled from '@emotion/styled'
 import { UnitedStates } from '../components/UnitedStates'
 import { Header } from '../components/Header'
@@ -7,7 +8,6 @@ import { Footer } from '../components/Footer'
 import { Modal } from '../components/Modal'
 import { initialize, logPageView } from '../lib/analytics'
 import { Data } from '../lib/utils'
-import { getData } from '../lib/api'
 import { BREAKPOINTS } from '../consts'
 
 const MapWrapper = styled.div`
@@ -26,10 +26,23 @@ const MapWrapper = styled.div`
   }
 `
 
-const Home = (props: { data: Data }) => {
-  const { data } = props
+const Home = () => {
+  const { data: usData, error: usDataError } = useSWR<Partial<Data>>('/us-data')
+  const { data: usChartData, error: usChartDataError } = useSWR<Partial<Data>>('/us-chart-data')
+  const { data: statesData, error: statesDataError } = useSWR<Partial<Data>>('/state-data')
   const [currentState, setCurrentState] = useState<string>('')
   const { width, height } = useWindowSize()
+
+  const data = useMemo(
+    () => ({
+      ...(statesData || {}),
+      usa: {
+        ...(usData && usData.usa ? usData.usa : {}),
+        ...(usChartData && usChartData.usa ? usChartData.usa : {}),
+      },
+    }),
+    [usData, usChartData, statesData]
+  ) as Partial<Data>
 
   useEffect(() => {
     initialize()
@@ -50,16 +63,13 @@ const Home = (props: { data: Data }) => {
             setCurrentState={setCurrentState}
           />
         </MapWrapper>
-        <Footer />
+        {data && data.usa ? (
+          <Footer />
+        ) : null}
       </div>
       <Modal currentState={currentState} clearState={() => setCurrentState('')} data={data} />
     </div>
   ) : null
-}
-
-Home.getInitialProps = async () => {
-  const data = await getData()
-  return { data }
 }
 
 export default Home
